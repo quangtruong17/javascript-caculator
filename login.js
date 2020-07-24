25b30591f3bdbbb2c3385c57f01655358b95104d
= <=>
2020-07-24 23:16:14 +0700

    init

---
+++ login.js
@@ -0,0 +1,12 @@
+
+function onLogin() {
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+    console.log('username :', username.value);
+    console.log('password :', password.value);
+    alert(`username: ${username.value}  passsword : ${password.value}`)
+    
+
+}
+
+// xong roi a
